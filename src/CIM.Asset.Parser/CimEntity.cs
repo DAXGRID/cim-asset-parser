05c1f810f87015df0b9889406bd2b6100c8729ff
Rune Nielsen <runeanielsen@live.dk>
2020-04-09 23:42:43 +0200

    adds the default logic for extracting CIM classes

---
+++ src/CIM.Asset.Parser/CimEntity.cs
@@ -0,0 +1,8 @@
+namespace CIM.Asset.Parser
+{
+    public class CimEntity
+    {
+        public string Name { get; set; }
+        public string XmiId { get; set; }
+    }
+}
---
+++ src/CIM.Asset.Parser/Tag.cs
@@ -0,0 +1,8 @@
+namespace CIM.Asset.Parser
+{
+    public class Tag
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}
