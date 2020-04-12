cfd5888d176439886a2a63ac4898ef04f9fc3a93
Rune Nielsen <runeanielsen@live.dk>
2020-04-12 21:27:27 +0200

    moves all magic strings to config class

---
+++ src/CIM.Asset.Parser/Xmi/EnterpriseArchitectConfig.cs
@@ -0,0 +1,18 @@
+namespace CIM.Asset.Parser.Xmi
+{
+    public static class EnterpriseArchitectConfig
+    {
+        public const string Generalization = "Generalization";
+        public const string TaggedValue = "TaggedValue";
+        public const string Tag = "tag";
+        public const string Documentation = "documentation";
+        public const string Value = "value";
+        public const string Attribute = "Attribute";
+        public const string Name = "name";
+        public const string Description = "description";
+        public const string Namespace = "namespace";
+        public const string Subtype = "subtype";
+        public const string XmiId = "xmi.id";
+        public const string Supertype = "supertype";
+    }
+}
--- src/CIM.Asset.Parser/Xmi/XmiParse.cs
+++ src/CIM.Asset.Parser/Xmi/XmiParse.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Xml.Linq;
 using System.Linq;
 using System.Text;
@@ -22,21 +21,22 @@ namespace CIM.Asset.Parser.Xmi
                 .Where(x => x.Name.LocalName == "Class");
 
             var generalizations = xElement.Descendants().OfType<XElement>()
-                            .Where(y => y.Name.LocalName == "Generalization");
+                .Where(y => y.Name.LocalName == EnterpriseArchitectConfig.Generalization);
 
             var cimEntities = classes?
                 .Select(x => new CimEntity
                 {
-                    Name = x.Attribute("name").Value?.ToString(),
-                    XmiId = x.Attribute("xmi.id").Value?.ToString(),
+                    Name = x.Attribute(EnterpriseArchitectConfig.Name).Value?.ToString(),
+                    XmiId = x.Attribute(EnterpriseArchitectConfig.XmiId).Value?.ToString(),
                     Description = x.Descendants().OfType<XElement>()
-                            .Where(y => y.Name.LocalName == "TaggedValue")
-                            .FirstOrDefault(y => y.Attribute("tag")?.Value?.ToString() == "documentation")?.Attribute("value")?.Value?.ToString(),
+                        .Where(y => y.Name.LocalName == EnterpriseArchitectConfig.Generalization)
+                        .FirstOrDefault(y => y.Attribute(EnterpriseArchitectConfig.Tag)?.Value?.ToString() == EnterpriseArchitectConfig.Documentation)?.Attribute(EnterpriseArchitectConfig.Value)?.Value?.ToString(),
                     Attributes = x.Descendants().OfType<XElement>()
-                            .Where(y => y.Name.LocalName == "Attribute")
-                        .Select(z => new Attribute { Name = z.Attribute("name").Value?.ToString(), Description = z.Descendants().OfType<XElement>().Where(t => t.Name.LocalName == "TaggedValue")?.FirstOrDefault(n => n.Attribute("tag")?.Value?.ToString() == "description")?.Attribute("value").Value?.ToString() }),
-                    Namespace = x.Attribute("namespace")?.Value?.ToString(),
-                    SuperType = generalizations.FirstOrDefault(y => y.Attribute("subtype")?.Value == x.Attribute("xmi.id").Value?.ToString())?.Attribute("supertype")?.Value.ToString()
+                        .Where(y => y.Name.LocalName == EnterpriseArchitectConfig.Attribute)
+                    .Select(z => new Attribute { Name = z.Attribute(EnterpriseArchitectConfig.Name).Value?.ToString(), Description = z.Descendants().OfType<XElement>().Where(t => t.Name.LocalName == EnterpriseArchitectConfig.TaggedValue)?.FirstOrDefault(n => n.Attribute(EnterpriseArchitectConfig.Tag)?.Value?.ToString() == EnterpriseArchitectConfig.Description)?.Attribute(EnterpriseArchitectConfig.Value).Value?.ToString() }),
+
+                    Namespace = x.Attribute(EnterpriseArchitectConfig.Namespace)?.Value?.ToString(),
+                    SuperType = generalizations.FirstOrDefault(y => y.Attribute(EnterpriseArchitectConfig.Subtype)?.Value == x.Attribute(EnterpriseArchitectConfig.XmiId).Value?.ToString())?.Attribute(EnterpriseArchitectConfig.Supertype)?.Value.ToString()
                 });
         }
     }
