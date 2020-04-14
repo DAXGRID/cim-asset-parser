611d9fca79cd3adecf00a195909b8cc65557c0f1
Rune Nielsen <runeanielsen@live.dk>
2020-04-14 20:22:20 +0200

    adds json file attribute

---
+++ test/CIM.Asset.Parser.Tests/JsonFileDataAttribute.cs
@@ -0,0 +1,58 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace CIM.Asset.Parser.Tests
+{
+
+    public class JsonFileDataAttribute : DataAttribute
+    {
+        private readonly string _filePath;
+        private readonly string _propertyName;
+
+        /// <summary>
+        /// Load data from a JSON file as the data source for a theory
+        /// </summary>
+        /// <param name="filePath">The absolute or relative path to the JSON file to load</param>
+        public JsonFileDataAttribute(string filePath)
+            : this(filePath, null) { }
+
+        /// <summary>
+        /// Load data from a JSON file as the data source for a theory
+        /// </summary>
+        /// <param name="filePath">The absolute or relative path to the JSON file to load</param>
+        /// <param name="propertyName">The name of the property on the JSON file that contains the data for the test</param>
+        public JsonFileDataAttribute(string filePath, string propertyName)
+        {
+            _filePath = filePath;
+            _propertyName = propertyName;
+        }
+
+        /// <inheritDoc />
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            if (testMethod == null) { throw new ArgumentNullException(nameof(testMethod)); }
+
+            var path = Path.IsPathRooted(_filePath)
+                ? _filePath
+                : Path.GetRelativePath(Directory.GetCurrentDirectory(), _filePath);
+
+            if (!File.Exists(path))
+                throw new ArgumentException($"Could not find file at path: {path}");
+
+            var fileData = File.ReadAllText(_filePath);
+
+            if (string.IsNullOrEmpty(_propertyName))
+                return JsonConvert.DeserializeObject<List<object[]>>(fileData);
+
+            // Only use the specified property as the data
+            var allData = JObject.Parse(fileData);
+            var data = allData[_propertyName];
+            return data.ToObject<List<object[]>>();
+        }
+    }
+}
