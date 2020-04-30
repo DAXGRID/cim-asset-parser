fa30c02da809993a073e255714f75ef08b942c25
Rune Nielsen <runeanielsen@live.dk>
2020-04-30 13:40:28 +0200

    json are now written as camelcase to file

--- src/CIM.Asset.Parser/FileIO/JsonFileWriter.cs
+++ src/CIM.Asset.Parser/FileIO/JsonFileWriter.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace CIM.Asset.Parser.FileIO
 {
@@ -7,7 +8,14 @@ namespace CIM.Asset.Parser.FileIO
     {
         public void Write(string path, object content, Formatting jsonFormatting)
         {
-            var json = JsonConvert.SerializeObject(content, jsonFormatting);
+            var serializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = jsonFormatting
+            };
+
+            var json = JsonConvert.SerializeObject(content, serializerSettings);
+
             File.WriteAllText(path, json);
         }
     }
